b23456d375fbf3e002047ad29c02efe76dfe9a10
Tubss2 <tobiasmallett@gmail.com>
2025-03-29 17:13:22 +1300

    BRUHH

--- main.js
+++ main.js
@@ -592,20 +592,18 @@ function calculate() {
 function displayFiringSolution() {
   const mission = fireMissions[selectedMissionIndex];
   const solution = mission.firingSolutions || {};
-  let degreesIndirect = (solution.milsIndirect || 0) * (360 / 6400);
-  let degreesDirect = (solution.milsDirect || 0) * (360 / 6400);
-  if (degreesIndirect > 60) {
-    degreesIndirect += 0.4;
-  }
+  let bearingMils = Math.round((solution.bearingDeg || 0) * (6400 / 360));
+  let indirectMils = Math.round(solution.milsIndirect || 0);
+  let directMils = Math.round(solution.milsDirect || 0);
   document.querySelector('.firing-solution').innerHTML = `
-    <div>Range: ${solution.horizontalDistance?.toFixed(2) || '00'}<br>Bearing: ${solution.bearingDeg || '00.00'}</div>
+    <div>Range: ${solution.horizontalDistance?.toFixed(2) || '00'} meters<br>Bearing: ${bearingMils || '0'} Mils</div>
     <div class="vertical-line"></div>
-    <div>Indirect Deg: ${degreesIndirect.toFixed(2) || '00.00'}<br>TOF: ${solution.tofIndirect || '00.00'}</div>
+    <div>Indirect Elevation: ${indirectMils || '0'} Mils<br>TOF: ${solution.tofIndirect || '00.00'} seconds</div>
     <div class="vertical-line"></div>
-    <div>Direct Deg: ${degreesDirect.toFixed(2) || '00.00'}<br>TOF: ${solution.tofDirect || '00.00'}</div>
-  `;
-}
-
+    <div>Direct Elevation: ${directMils || '0'} Mils<br>TOF: ${solution.tofDirect || '00.00'} seconds</div>
+  `; // Added closing backtick and semicolon
+} // Added closing brace
+// Attach all functions to window AFTER they are defined
 window.start = start;
 window.addNewMission = addNewMission;
 window.renameMission = renameMission;
